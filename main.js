5c711bf47778dcbf8e31e9891360dbaef6cab31a
Sergii Tsekhmister <serhiitsekhmister@gmail.com>
2023-10-26 14:27:55 +0300

    test

---
+++ main.js
@@ -0,0 +1,9 @@
+// Using the jquery library
+$(".accordion-content").css("display", "none");
+
+$(".accordion-title").click(function () {
+  $(".accordion-title").not(this).removeClass("open");
+  $(".accordion-title").not(this).next().slideUp(300);
+  $(this).toggleClass("open");
+  $(this).next().slideToggle(300);
+});
