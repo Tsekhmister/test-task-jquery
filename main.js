67b69edf89aeab61cd7991cd0c03c04f73d116fb
Sergii Tsekhmister <serhiitsekhmister@gmail.com>
2023-10-29 07:21:15 +0200

    feat: add all changes, finish the task

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Using the jquery library
 $(".accordion-content").css("display", "none");
 
 $(".accordion-title").click(function () {
@@ -6,4 +5,35 @@ $(".accordion-title").click(function () {
   $(".accordion-title").not(this).next().slideUp(300);
   $(this).toggleClass("open");
   $(this).next().slideToggle(300);
+});
+
+$(document).ready(function () {
+  $(".carousel-wrapper").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    prevArrow: $(".left-arrow"),
+    nextArrow: $(".right-arrow"),
+    responsive: [
+      {
+        breakpoint: 1124,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
 });
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Test
+
+  - Layout the page according to design
+  - Make adaptations for resolutions of 1024 and for any resolution less than 500
+  - Technologies used Jquery, Slick and CSS
+  ## [Solution DEMO](https://tsekhmister.github.io/test-task-jquery/)
